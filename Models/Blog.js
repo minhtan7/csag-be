6e558f9188a97ab090626532f9ecf4b756ef469a
Your Name <you@example.com>
2021-08-08 20:08:33 +0700

    timestamp

--- Models/Blog.js
+++ Models/Blog.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const blogShema = Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  author: { type: String, required: true },
-  isDeleted: { type: Boolean, default: false },
-});
+const blogShema = Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: String, required: true },
+    isDeleted: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 const Blog = mongoose.model("Blog", blogShema);
 module.exports = Blog;
--- Models/Order.js
+++ Models/Order.js
@@ -1,24 +1,27 @@
 const mogoose = require("mongoose");
 const Schema = mogoose.Schema;
 
-const orderSchema = new Schema({
-  from: { type: Schema.Types.ObjectId, required: true, ref: "User" },
-  to: { type: Schema.Types.ObjectId, required: true, ref: "User" },
-  shipperId: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
+const orderSchema = new Schema(
+  {
+    from: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    to: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    shipperId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    itemId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Item",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accept", "delivering", "receive", "done"],
+    },
   },
-  itemId: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "Item",
-  },
-  status: {
-    type: String,
-    enum: ["pending", "accept", "delivering", "receive", "done"],
-  },
-});
+  { timestamps: true }
+);
 const Order = mongoose.model("Order", orderSchema);
 
 module.exports = Order;
