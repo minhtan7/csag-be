01d544dba79c65c44224838ef515e918d5cc4cf8
Nguyen Lien <nguyen.k.lien98@gmail.com>
2021-08-09 21:47:12 +0700

    matching controller

--- routes/form.api.js
+++ routes/form.api.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const formController = require("../controllers/form.controller");
+const authMiddleware = require("../middlewares/authentication");
 
 const router = express.Router();
 
 /**
- * @route POST api/item
+ * @route POST api/form/
  * @description Create item
  * @acces Public
  *
  */
 router.post("/", formController.createForm);
+
+/**
+ * @route GET api/form/matching
+ * @description get matched forms
+ * @acces giver
+ *
+ */
+router.get("/matching",authMiddleware.loginRequired, formController.matchingReceiver )
 
 
 
