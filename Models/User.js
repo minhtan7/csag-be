f544e7ef6546882cedb135e9d158317a976a1c05
eatmoarrice <63140320+eatmoarrice@users.noreply.github.com>
2021-08-10 14:08:45 +0700

    faker

--- Models/User.js
+++ Models/User.js
@@ -1,28 +1,28 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, unique: true, required: true },
-    password: { type: String },
-    phone: {type: Number, require: true, unique: true},
-    role: {
-      type: String,
-      required: true,
-      enum: ["giver", "recipient", "shipper", "admin"],
-    },
-    deliveryMethod: {
-      type: String,
-      required: true,
-      enum: ["pickUp", "delivery", "needShipper"],
-    },
-    address: { type: String, required: true },
-    geocode: {lat:{type:Number},lng:{type:Number}},
-    city:{type: String, required: true},
-    images: [{ imageUrl: { type: String } }],
-  },
-  { timestamps: true }
+	{
+		name: { type: String, required: true },
+		email: { type: String, unique: true },
+		password: { type: String, required: true },
+		phone: { type: String, required: true, unique: true },
+		role: {
+			type: String,
+			// required: true,
+			enum: ['giver', 'recipient', 'shipper', 'admin'],
+		},
+		deliveryMethod: {
+			type: String,
+			// required: true,
+			enum: ['pickUp', 'delivery', 'needShipper'],
+		},
+		address: { type: String, required: true },
+		geocode: { lat: { type: Number }, lng: { type: Number } },
+		city: { type: String, required: true },
+		images: [{ imageUrl: { type: String } }],
+	},
+	{ timestamps: true }
 );
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
